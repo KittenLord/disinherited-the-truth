550856d9d361a2a91029f9d38293634ff8dc14da
Vanya Braslavets <vanya.braslavets@gmail.com>
2024-07-01 05:25:47 +0300

    added basics

---
+++ DisinheritedGambling/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float MovementSpeed;
+    [SerializeField] private float JumpForce;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Camera cam;
+
+    private Vector3 direction;
+
+    private bool moveJump = false;
+    private bool jumpState = false;
+    private bool touchesGround = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        var axis = Input.GetAxisRaw("Horizontal");
+        direction = new Vector2(axis, 0);
+        moveJump = axis != 0;
+        if(!moveJump)
+        {
+            animator.Play("Idle");
+        }
+
+        var mouseDirection = Mathf.Sign((cam.ScreenToWorldPoint(Input.mousePosition) - transform.position).x);
+        if(axis != 0) transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * axis, transform.localScale.y);
+        else transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * mouseDirection, transform.localScale.y);
+    }
+
+    void FixedUpdate() 
+    {
+        rb.velocity = new Vector2(direction.x, rb.velocity.y);
+        if(touchesGround && moveJump)
+        {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            if(jumpState = !jumpState)
+                 animator.Play("Jump1");
+            else animator.Play("Jump2");
+
+            touchesGround = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        UnityEngine.Debug.Log($"ground");
+        if(col.transform.tag == "Ground") 
+        {
+            touchesGround = true;
+        }
+    }
+}
